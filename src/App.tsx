1a1df24202423a285504c0e0bddafa3dd4b9284a
Vikas Agarwal <vikas.aga23@gmail.com>
2023-01-29 09:44:18 -0500

    Removed unused code

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,7 @@
-import ReactDOM from "react-dom/client";
 import React from 'react';
 import './App.css';
 import { PFICCalculator } from './Components/PFICCalculator';
-import { BrowserRouter, Routes, Route, Outlet, Link } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Layout } from "./layout";
 import { License } from "./Components/License";
 
